254c1fd7c32f5ba07299f17e46142cb666528dac
AsyncX <1158271328@qq.com>
2023-12-20 15:49:16 +0800

    done!

--- bookmarks.js
+++ bookmarks.js
@@ -1,17 +1,25 @@
 // Note: having length != 4 will mess with layout based on how the site is styled
 const bookmarks = [
   {
-    title: "📖Pages",
+    title: "Pages📖",
     links: [
       { name: "Blog", url: "https://hi.asyncx.top" },
       { name: "Docsify", url: "https://home.asyncx.top" },
     ],
   },
   {
-    title: "📱Social",
+    title: "Project🖥️",
     links: [
-      { name: "Mail", url: "mailto:contact@asyncx.top" },
-      // { name: "Facebook", url: "https://facebook.com" },
+      { name: "Docsify-Darcular", url: "https://github.com/A5yncX/docsify-theme-darcula" },
+      { name: "Notion-To-Beancount", url: "https://github.com/A5yncX/notion-to-beancount" },
+      { name: "Sourcecode", url: "https://github.com/A5yncX/homepage" },
+    ],
+  },
+  {
+    title: "Social📱",
+    links: [
+      { name: "Github", url: "https://github.com/A5yncX" },
+      { name: "Mail📮", url: "mailto:contact@asyncx.top" },
     ],
   },
 ];
